92cba630e685fc119cfcd053d2294d907d51d0ac
suba466 <subashreethavaselvan@gmail.com>
2025-08-31 19:02:56 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Urban from './Urban.jsx';
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <Urban/>
+      <Urban/>
     </BrowserRouter>
   </StrictMode>
-)
+);
