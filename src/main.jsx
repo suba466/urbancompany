5539cfc1b670d07eaa027ce9261ea1bff509e785
suba466 <subashreethavaselvan@gmail.com>
2025-09-03 15:25:54 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Urban from './Urban.jsx';
-//import Reducertask from './Reducertask.jsx';<Reducertask/>
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <BrowserRouter><Urban/></BrowserRouter>
