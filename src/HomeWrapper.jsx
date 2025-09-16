14e83da9b63f1d05e6011e4823b5e0f1fca4e9a2
suba466 <subashreethavaselvan@gmail.com>
2025-09-16 15:41:08 +0530

    Delete src/HomeWrapper.jsx

--- src/HomeWrapper.jsx
+++
@@ -1,8 +0,0 @@
-import { useAuth } from "./Create.jsx";
-import Home3 from "./Home3.jsx";
-import Home4 from "./Home4.jsx";
-function HomeWrapper() {
-  const { showHome3 } = useAuth();
-  return showHome3 ? <Home3 /> : <Home4 />;
-}
-export default HomeWrapper;
