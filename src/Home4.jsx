16db27d79adb5fdf36764189ebae4d33b567996e
suba466 <subashreethavaselvan@gmail.com>
2025-09-16 15:41:01 +0530

    Delete src/Home4.jsx

--- src/Home4.jsx
+++
@@ -1,9 +0,0 @@
-import back from './assets/back.jpg'
-function Home4(){
-    return(
-        <div style={{textAlign:"center"}}> <br />
-            <img src={back} style={{height:"450px", width:"50%"}}/>
-            <h1>We welcome you wholeheatedly!!!!</h1>
-        </div>
-    )
-}export default Home4
