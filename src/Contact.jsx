761d1d611da73feac78e2a16086fd5af3f72da04
suba466 <subashreethavaselvan@gmail.com>
2025-09-16 15:40:16 +0530

    Delete src/Contact.jsx

--- src/Contact.jsx
+++
@@ -1,8 +0,0 @@
-import img1 from './assets/food6.jpg'
-function Contact(){
-    return(
-        <div style={{textAlign:"center"}}>
-        <h2>Welcome to the contact page!!</h2>
-        <img src={img1} alt="" /></div>
-    )
-}export default Contact;
