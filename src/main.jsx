7eb6d00c0ba298300e250fdc7e0adbbb458ae2c4
suba466 <subashreethavaselvan@gmail.com>
2025-09-16 15:42:47 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Regis from './Regis.jsx';
-//import Useparam from './Useparam.jsx';
-//import Articles from './Articles.jsx';
-//import Uses from './Uses.jsx';
-//import Nestedrouting from './Nestedrouting.jsx';
 import Urban from './Urban'
-//import Protect from './Protect.jsx';
-import Authen from './Authen.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
