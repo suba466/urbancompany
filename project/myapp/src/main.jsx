d1ad7eb6cce7aecc08b94cfd626b4e8e574f4c9d
Suba Shree <subashreethavaselvan@gmail.com>
2026-02-05 19:49:44 +0530

    main

--- project/myapp/src/main.jsx
+++ project/myapp/src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Urban from './Urban.jsx';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
-    <BrowserRouter basename={import.meta.env.BASE_URL} future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Urban />
-    </BrowserRouter>
-
+    </HashRouter>
   </StrictMode>
 );
