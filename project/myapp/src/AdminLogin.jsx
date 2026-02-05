5e3b47e5b695187f40926b51bfb763af20e57b07
Suba Shree <subashreethavaselvan@gmail.com>
2026-02-05 13:54:19 +0530

    admin

--- project/myapp/src/AdminLogin.jsx
+++ project/myapp/src/AdminLogin.jsx
@@ -114,7 +114,7 @@ function AdminLogin({ onLogin }) {
 
                     <h3 className="text-center mb-1 fw-bold">Urban Company</h3>
                     <p className="text-center mb-0 opacity-75" style={{ fontSize: "1rem" }}>
-                      Enterprise Administration Portal
+                       Administration Portal
                     </p>
                   </div>
                 </Col>
